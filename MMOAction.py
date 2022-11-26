8bc2d9b1d8706dfd10ce8c5fb3e50410a0ff1adf
None <None>
2022-11-26 06:57:10 -0300

    Initial commit

---
+++ MMOAction.py
@@ -0,0 +1,85 @@
+import sys
+import pyautogui
+import subprocess
+import keyboard
+import time
+from screeninfo import get_monitors
+
+on = False
+
+middle = None
+
+height = 0
+width = 0
+
+tick = 0.1
+
+for m in get_monitors():
+    if m.is_primary:
+        height = m.height / 2
+        width = m.width / 2
+
+
+def quick_exit():
+    if keyboard.is_pressed("Shift"):
+        if keyboard.is_pressed("Esc"):
+            pyautogui.mouseUp(button="right")
+            sys.exit()
+
+
+def cursor():
+    global middle
+
+    option = input("Always move cursor to the middle of the screen? Y/N"
+                   "\nInput: ")
+
+    if option.upper() == "Y":
+        middle = True
+    elif option.upper() == "N":
+        middle = False
+    else:
+        subprocess.run("cls", shell=True)
+        cursor()
+
+    subprocess.run("cls", shell=True)
+
+
+cursor()
+
+print("MMO Action is now running!\n")
+print("HOW TO USE:\n")
+print("Hold SHIFT + ALT to lock your right mouse button into action camera.\n")
+print("Hold SHIFT + CTRL to release your right mouse button from action camera.\n")
+print("Holding CAPSLOCK while the right mouse button is locked into action camera, will release it so you can interact "
+      "with NPCs and objects in game. Releasing CAPSLOCK will lock your mouse back into action camera.\n")
+print("Hold SHIFT + ESC to kill the program and release your mouse.")
+
+while True:
+    while on:
+        if not keyboard.is_pressed("Capslock"):
+            if pyautogui.position().x != width and pyautogui.position().y != height and middle:
+                pyautogui.moveTo(width, height)
+
+            pyautogui.mouseDown(button="right")
+
+        if keyboard.is_pressed("Capslock"):
+            pyautogui.mouseUp(button="right")
+
+        if keyboard.is_pressed("Shift"):
+            if keyboard.is_pressed("Ctrl"):
+                pyautogui.mouseUp(button="right")
+                on = False
+        if not on:
+            break
+
+        quick_exit()
+
+        time.sleep(tick)
+
+    if keyboard.is_pressed("Shift") and not on:
+        if keyboard.is_pressed("Alt"):
+            on = True
+
+    quick_exit()
+
+    time.sleep(tick)
---
+++ README.md
@@ -0,0 +1,16 @@
+# MMOAction
+This tiny program tries to simulate an action camera on MMOs. So instead of holding your right mouse button to move your camera and your character, this tool will do it for you!
+
+# How to use
+
+Hold SHIFT + ALT to lock your right mouse button into action camera.
+
+Hold SHIFT + CTRL to release your right mouse button from action camera.
+
+Holding CAPSLOCK while the right mouse button is locked into action camera, will release it so you can interact with NPCs and objects in game. Releasing CAPSLOCK will lock your mouse back into action camera.
+
+Hold SHIFT + ESC to kill the program and release your mouse.
+
+# Download
+
+You can download the program directly from this link:
